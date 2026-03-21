67d7dd1b7181f1b902203608e66a0bd7950af397
Pontus Persson <86252969+VikingenPersson@users.noreply.github.com>
2026-03-21 10:52:50 +0100

    Finish foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    let sum = 0;
+    let low = a < b ? a : b;
+    const high = a > b ? a : b;
+    for (low; low <= high; low++) sum += low;
+    return sum;
 };
 
 // Do not edit below this line
