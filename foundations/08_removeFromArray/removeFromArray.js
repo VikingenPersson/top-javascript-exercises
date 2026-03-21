ba469737b2a0d97626a70d59b474c123d0b1a6fe
Pontus Persson <86252969+VikingenPersson@users.noreply.github.com>
2026-03-21 10:38:08 +0100

    Finish foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (let item of itemsToRemove){
+        while (array.includes(item)){
+            array.splice(array.findIndex(arrayItem => arrayItem === item), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
