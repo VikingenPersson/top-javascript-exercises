f211cf54d114c0c413d9d9328518ded3e442dc0a
Pontus Persson <86252969+VikingenPersson@users.noreply.github.com>
2026-03-21 11:15:56 +0100

    Finish foundations/11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitTemperature) {
+  const celsiusTemperature = (farenheitTemperature - 32) * (5/9);
+  if (Number.isInteger(celsiusTemperature)){
+    return celsiusTemperature
+  }
+  return Number(Number.parseFloat(celsiusTemperature).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  const farenheitTemperature = celsiusTemperature * (9/5) + 32;
+
+  if (Number.isInteger(farenheitTemperature)){
+    return farenheitTemperature;
+  }
+  return Number(Number.parseFloat(farenheitTemperature).toFixed(1));
 };
 
 // Do not edit below this line
